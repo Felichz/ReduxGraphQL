30bcee03000bd37d69e33c57282c4911d7c851f3
Felichz <andersson.fx@gmail.com>
2020-08-20 23:12:38 -0300

    Updates

---
+++ precache-manifest.d7bddd2c44b98460cc754d5203bee5da.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "faa1fcfbf127dbb3d983caa6582f675e",
+    "url": "./index.html"
+  },
+  {
+    "revision": "87b35602b5569e6e065d",
+    "url": "./static/css/2.5c1ee032.chunk.css"
+  },
+  {
+    "revision": "f53fb6fc94fa182e6ff5",
+    "url": "./static/css/main.7b7f78aa.chunk.css"
+  },
+  {
+    "revision": "87b35602b5569e6e065d",
+    "url": "./static/js/2.295a930c.chunk.js"
+  },
+  {
+    "revision": "54b38e645acf525931f7099a3a830e44",
+    "url": "./static/js/2.295a930c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f53fb6fc94fa182e6ff5",
+    "url": "./static/js/main.b78f3cd6.chunk.js"
+  },
+  {
+    "revision": "40c18731e5950c61515f",
+    "url": "./static/js/runtime-main.13588be4.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "./static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "./static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "./static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "./static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "./static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/ReduxGraphQL/precache-manifest.ff10da780afddaffa58123f39aa2dc76.js"
+  "./precache-manifest.d7bddd2c44b98460cc754d5203bee5da.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/ReduxGraphQL/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
